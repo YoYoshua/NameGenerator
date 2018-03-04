713ae20bee7fb8fdf6c15a22fd37c70cc368d9e0
Marcin Prystupa <32766539+YoYoshua@users.noreply.github.com>
2018-03-04 18:56:08 +0100

    Added working name generator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
-public class Main {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        File nameFile = new File("namefile.txt");
+        File surnameFile = new File("surnamefile.txt");
+
+        List<String> nameList = new ArrayList<String>();
+        List<String> surnameList = new ArrayList<String>();
+
+        FileHandler handler = new FileHandler();
+        NameGenerator generator = new NameGenerator();
+
+        nameList = handler.readFile(nameFile);
+        surnameList = handler.readFile(surnameFile);
+
+        System.out.println(generator.getRandomName(nameList, surnameList));
+
     }
 }
---
+++ src/com/company/NameGenerator.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.List;
+import java.util.Random;
+
+public class NameGenerator
+{
+    //TODO: Generating random name and surname based on list from FileHandler class
+    public String getRandomName(List<String> nameList, List<String> surnameList)
+    {
+
+        if (nameList.size() == 0 || surnameList.size() == 0)
+        {
+            return "Something is wrong!";
+        }
+        else
+        {
+            //Create random generator
+            Random randomName = new Random();
+            Random randomSurname = new Random();
+
+            //Return
+            try
+            {
+                String result = nameList.get(randomName.nextInt(nameList.size())) + " " +
+                        surnameList.get(randomSurname.nextInt(surnameList.size()));
+                return result;
+            }
+            catch(ArrayIndexOutOfBoundsException e)
+            {
+                e.printStackTrace();
+                return "Something is wrong!";
+            }
+        }
+    }
+}
